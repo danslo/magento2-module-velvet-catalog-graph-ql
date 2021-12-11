c983a36175d9841d61c5c54503cd9d9aa01090cd
Daniel Sloof <daniel@wearejh.com>
2021-12-11 16:39:35 +0100

    attribute data in product view resolver

--- Model/Resolver/Product/View.php
+++ Model/Resolver/Product/View.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Danslo\VelvetCatalogGraphQl\Model\Resolver\Product;
 
 use Danslo\VelvetGraphQl\Api\AdminAuthorizationInterface;
+use Magento\Catalog\Api\ProductAttributeGroupRepositoryInterface;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Eav\Model\Attribute;
+use Magento\Eav\Model\Entity\Attribute\Group;
+use Magento\Eav\Model\Entity\Attribute\Option;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SortOrderBuilder;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
@@ -14,15 +21,62 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 class View implements ResolverInterface, AdminAuthorizationInterface
 {
     private ProductRepositoryInterface $productRepository;
+    private ProductAttributeGroupRepositoryInterface $attributeGroupRepository;
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+    private SortOrderBuilder $sortOrderBuilder;
+    private ProductAttributeRepositoryInterface $attributeRepository;
 
     public function getResource(): string
     {
         return 'Magento_Catalog::products';
     }
 
-    public function __construct(ProductRepositoryInterface $productRepository)
-    {
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        ProductAttributeGroupRepositoryInterface $attributeGroupRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SortOrderBuilder $sortOrderBuilder,
+        ProductAttributeRepositoryInterface $attributeRepository
+    ) {
         $this->productRepository = $productRepository;
+        $this->attributeGroupRepository = $attributeGroupRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->sortOrderBuilder = $sortOrderBuilder;
+        $this->attributeRepository = $attributeRepository;
+    }
+
+    private function getAttributeGroupsForSet(int $attributeSetId): array
+    {
+        return $this->attributeGroupRepository->getList(
+            $this->searchCriteriaBuilder
+                ->addFilter('attribute_set_id', $attributeSetId)
+                ->addSortOrder($this->sortOrderBuilder->setField('sort_order')->setAscendingDirection()->create())
+                ->create()
+        )->getItems();
+    }
+
+    private function getAttributesForGroups(array $attributeGroupIds): array
+    {
+        return $this->attributeRepository->getList(
+            $this->searchCriteriaBuilder
+                ->addFilter('attribute_group_id', $attributeGroupIds, 'in')
+                ->addSortOrder($this->sortOrderBuilder->setField('sort_order')->setAscendingDirection()->create())
+                ->create()
+        )->getItems();
+    }
+
+    private function getFlattenedOptions(array $inputOptions): array
+    {
+        $outputOptions = [];
+
+        /** @var Option $option */
+        foreach ($inputOptions as $option) {
+            $outputOptions[] = [
+                'value' => $option->getValue(),
+                'label' => (string) $option->getLabel()
+            ];
+        }
+        return $outputOptions;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
@@ -33,12 +87,43 @@ class View implements ResolverInterface, AdminAuthorizationInterface
         }
 
         $product = $this->productRepository->getById($productId);
+
+        $attributeGroups = $this->getAttributeGroupsForSet((int) $product->getAttributeSetId());
+
+        $attributesByGroupId = [];
+        /** @var Attribute $attribute */
+        foreach ($this->getAttributesForGroups(array_keys($attributeGroups)) as $attribute) {
+            $attributeValue = $product->getData($attribute->getAttributeCode());
+            if (is_array($attributeValue)) {
+                // todo: handle multidimensional
+                continue;
+            }
+
+            $attributesByGroupId[$attribute->getAttributeGroupId()][$attribute->getAttributeId()] = [
+                'label' => $attribute->getName(),
+                'type' => $attribute->getFrontendInput(),
+                'value' => $attributeValue,
+                'code' => $attribute->getAttributeCode(),
+                'options' => $this->getFlattenedOptions($attribute->getOptions())
+            ];
+        }
+
+        $attributeGroupsData = [];
+
+        /** @var Group $attributeGroup */
+        foreach ($attributeGroups as $attributeGroup) {
+            $attributeGroupsData[] = [
+                'label' => $attributeGroup->getAttributeGroupName(),
+                'attributes' => $attributesByGroupId[$attributeGroup->getAttributeGroupId()] ?? []
+            ];
+        }
+
         return [
             'entity_id' => $product->getId(),
             'attribute_set_id' => $product->getAttributeSetId(),
             'type_id' => $product->getTypeId(),
             'sku' => $product->getSku(),
-            'attribute_groups' => []
+            'attribute_groups' => $attributeGroupsData
         ];
     }
 }
