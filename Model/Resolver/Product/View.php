90183c47bccfe975f88bc5e8df19f19159c941a6
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-11 15:43:03 +0100

    use product table reader

--- Model/Resolver/Product/View.php
+++ Model/Resolver/Product/View.php
@@ -5,19 +5,40 @@ declare(strict_types=1);
 namespace Danslo\VelvetCatalogGraphQl\Model\Resolver\Product;
 
 use Danslo\VelvetGraphQl\Api\AdminAuthorizationInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
 class View implements ResolverInterface, AdminAuthorizationInterface
 {
+    private ProductRepositoryInterface $productRepository;
+
     public function getResource(): string
     {
         return 'Magento_Catalog::products';
     }
 
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        // TODO: Implement resolve() method.
+        $productId = $args['product_id'] ?? null;
+        if ($productId === null) {
+            throw new GraphQlInputException(__('Product ID must be specified.'));
+        }
+
+        $product = $this->productRepository->getById($productId);
+        return [
+            'entity_id' => $product->getId(),
+            'attribute_set_id' => $product->getAttributeSetId(),
+            'type_id' => $product->getTypeId(),
+            'sku' => $product->getSku(),
+            'attribute_groups' => []
+        ];
     }
 }
